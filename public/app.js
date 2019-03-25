dfc5ae25328aed35451e6a0baec7ce3b589b3165
Daffolap-386 <tushar.garg@daffodilsw.com>
2019-03-25 19:29:49 +0530

    resolving icons issues

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@ import moment from 'moment';
 import chrome from 'ui/chrome';
 import { uiModules } from 'ui/modules';
 import uiRoutes from 'ui/routes';
-
+import 'ui/autoload/modules';
 import 'ui/autoload/styles';
 import './less/main.less';
 import template from './templates/index.html';
